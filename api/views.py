a6cfcdaaa88b92dfe153fe3e992031081f41a52b
vineeth0791 <vineethkumar0791@gmail.com>
2019-01-09 19:40:19 +0530

    api

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 import json as simplejson
 from django.http import HttpResponse,JsonResponse
-from blog.models import Post 
+from blog.models import Post
 import json
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -12,16 +12,22 @@ import json
 def first(request):
 	#result = json.loads(request.body)
 	#body = json.loads(request.body)
-	return JsonResponse({'request_get':request.GET,
-		'request_post':request.data})
-	postData = Post.objects.filter(id=2)
 
+
+    if request.method == "GET":
+    	idValue = request.GET.get('id')
+    else:
+    	idValue = request.POST.get('id')
+
+    postData = Post.objects.filter(id=idValue)
 	#postData = Post.objects.all()
-	if(len(postData)>=1):
-		data = list(postData.values('title'))
-		data =JsonResponse({'data':data})
-	else:
-		data = JsonResponse({'statusCode':1,'status':'No blog found',
+
+    if(len(postData)>=1):
+        data = list(postData.values('title'))
+        data =JsonResponse({'data':data})
+    else:
+	    data = JsonResponse({'statusCode':1,'status':'No blog found',
 			'len':len(postData),'id':request.data})
-	return data
+
+    return data
 	#return True
